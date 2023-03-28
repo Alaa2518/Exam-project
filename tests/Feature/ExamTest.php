774e990917a1c76100844a07954c97069356b8a7
Alaa2518 <52606160+Alaa2518@users.noreply.github.com>
2023-03-28 13:04:18 +0200

    questions test matrix

--- tests/Feature/ExamTest.php
+++ tests/Feature/ExamTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ExamTest extends TestCase
@@ -13,10 +16,25 @@ class ExamTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_login_super_admin()
     {
-        $response = $this->get('/');
+        User::create([
+            'name' => 'moka',
+            'email' => 'superAdmin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make("password"),
+            // password
+            'remember_token' => Str::random(10),
+            'code' => null,
+        ])->assignRole('superAdmin');
 
-        $response->assertStatus(200);
+        $response = $this->post('/login', [
+            'email' => 'superAdmin@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(302);
+        $response->isRedirect('/exam/index');
+
     }
 }
--- tests/Feature/QuestionTest.php
+++ tests/Feature/QuestionTest.php
@@ -3,17 +3,34 @@
 namespace Tests\Feature;
 
 use App\Models\Question;
+
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class QuestionTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_redirect_if_not_authntecated()
+    private $user ;
+    private $question;
+
+    private function set_up(){
+        $this->user = User::create([
+            'name' => 'admin',
+            'email' => 'superAdmin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make("password"), // password
+            'remember_token' => Str::random(10),
+            'code' => null,
+        ])->assignRole('superAdmin');
+
+        $this->question = Question::factory(1)->create();
+    }
+    public function test_redirect_if_not_authntecated_to_show_question()
     { // this function is not in this class
         $response = $this->get('/question/index');
 
@@ -21,109 +38,97 @@ class QuestionTest extends TestCase
         $response->assertRedirect('/login');
     }
 
-    // public function test_questions_page_not_empty(){
+    public function test_question_deleted(){
 
-    //     $user = User::factory()->create();
-    //     $questions =Question::all();
-    //     $response = $this->actingAs($user)->get('/question/index');
-    //     $response->assertStatus(200);
-    //     $response->assertDontSee(__('No Questions'));
-    //     $response->assertViewHas('questions',function($collection)use ($questions){
-    //         return $collection->contains($questions);
-    //     });
+        $this->set_up();
 
-    // }
+        $this->assertDatabaseHas('questions',$this->question);
+        $questions = Question::find(1);
 
-    // public function test_question_deleted(){
+        $response = $this->actingAs($this->user)->delete('options/delete/1/'.$questions['id']);
+        $response->assertStatus(200);
+        $response->assertStatus(302);
+        $response->assertRedirect('/questions/index/');
+        $this->assertDatabaseMissing('questions',$this->question);
 
-    //     $user = User::factory()->create();
+    }
 
-    //     $questions = Question::all();
-    //     $response = $this->actingAs($user)->get('/question/index');
-    //     $response->assertStatus(200);
-    //     $response->assertDontSee(__('No Questions'));
-    //     $response->assertViewHas('questions', function ($collection) use ($questions) {
-    //         return $collection->contains($questions);
-    //     });
-    // }
+    public function test_questions_page_not_empty(){
 
-    // public function test_login_super_admin(){
-    //     User::factory()->create();
+        $this->set_up();
 
-    //     $response = $this->post('/login',[
-    //         'email'=>'superAdmin@gmail.com',
-    //         'password'=>'password'
-    //     ]);
+        $questions =Question::all();
+        $response = $this->actingAs($this->user)->get('/question/index');
+        $response->assertStatus(200);
+        $response->assertDontSee(__('No Questions'));
+        $response->assertViewHas('questions',function($collection)use ($questions){
+            return $collection->contains($questions);
+        });
 
-    //     $response->assertStatus(302);
-    //     $response->isRedirect('/exam/index');
+    }
 
-    // }
+    public function test_add_question_edit()
+    {
+        $this->set_up();
 
 
-    // public function test_add_question_edit()
-    // {
-    //     $user = User::factory()->create();
-    //     $question= Question::factory()->create();
-    //     $response = $this->actingAs($user)->get('/question/edit/'.$question['id']);
-    //     $response->assertStatus(200);
+        $response = $this->actingAs($this->user)->get('/question/edit/'.$this->question['id']);
+        $response->assertStatus(200);
+        $response->assertSee('value="'.$this->question['body'].'"',false);
+        $response->assertSee('value="' . $this->question['type'] . '"', false);
+    }
 
-    //     $response->assertSee('value="'.$question['body'].'"',false);
-    //     $response->assertSee('value="' . $question['type'] . '"', false);
-    // }
+    public function test_add_question_error_redirects_update()
+    {
+        $this->set_up();
 
-    // public function test_add_question_error_redirects_update()
-    // {
-    //     $user = User::factory()->create();
-    //     $question = Question::factory()->create();
-    //     $response = $this->actingAs($user)->put('/question/edit/' . $question['id'],[
-    //         'body'=>'',
-    //         'type'=> ''
-    //     ]);
-    //     $response->assertStatus(302);
-    //     $response->assertSessionHasErrors(['body','type']);
-    //     $response->assertInvalid(['body']);
+        $response = $this->actingAs($this->user)->put('/question/edit/' . $this->question['id'],[
+            'body'=>'',
+            'type'=> ''
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['body','type']);
+        $response->assertInvalid(['body']);
+    }
 
-    // }
+    public function test_add_questions_to_database()
+    {
+        $this->set_up();
 
-    // public function test_add_questions_to_database()
-    // {
-    //     User::factory()->create();
-    //     $qustion = [
-    //         'exam_id' => 3,
-    //         'question_type' => 'true_or_false',
-    //         'body' => 'asdfasfiaosd asdlkfaia akslaiena',
+        $response = $this->post('/login', [
+            'email' => 'superAdmin@gmail.com',
+            'password' => 'password'
+        ]);
 
-    //     ];
+        $response->assertStatus(302);
+        $response->assertRedirect('/question/create/3');
 
-    //     $response = $this->post('/login', [
-    //         'email' => 'superAdmin@gmail.com',
-    //         'password' => 'password'
-    //     ]);
+        $this->assertDatabaseHas('question', $this->question[0]);
 
-    //     $response->assertStatus(302);
-    //     $response->assertRedirect('/question/create/3');
+        $lastQuestion = Question::latest()->first();
 
-    //     $this->assertDatabaseHas('question', $qustion);
+        $this->assertEquals($lastQuestion->body, $this->question['body']);
+        $this->assertEquals($lastQuestion->question_type, $this->question['question_type']);
 
-    //     $lastQuestion = Question::latest()->first();
+    }
 
-    //     $this->assertEquals($lastQuestion->body, $qustion['body']);
-    //     $this->assertEquals($lastQuestion->question_type, $qustion['question_type']);
+    public function test_question_delete_successul(){
+        $this->set_up();
 
-    // }
+        $question = $this->question;
+        $response = $this->actingAs($this->user)->delete('/question/delete/' . $question['id']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/questions/index/');
 
-    // public function test_question_delete_successul(){
-    //     $user = User::factory()->create();
-    //     $question = Question::factory()->create();
-    //     $response = $this->actingAs($user)->delete('/question/delete/' . $question['id']);
-    //     $response->assertStatus(302);
-    //     $response->assertRedirect('/questions/index/');
+        $this->assertDatabaseMissing('questions',$question[0]);
+         $response->assertDontSee(__('No Questions'));
+        $response->assertViewHas('questions', function ($collection) use($question) {
+            return $collection->contains($question);
+        });
 
-    //     $this->assertDatabaseMissing('questions',$question[0]);
+    }
 
 
-    // }
 
 
 
