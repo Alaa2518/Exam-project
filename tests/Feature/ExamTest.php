ba7d23a64e3eb1be51c55572cd6678d8b29f7546
Alaa2518 <52606160+Alaa2518@users.noreply.github.com>
2023-03-30 16:23:40 +0200

    test exam

--- tests/Feature/ExamTest.php
+++ tests/Feature/ExamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Exam;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,15 +12,15 @@ use Tests\TestCase;
 
 class ExamTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_login_super_admin()
+    use RefreshDatabase;
+
+    private $user;
+    private $exam ;
+
+    private function set_up()
     {
-        User::create([
-            'name' => 'moka',
+        $this->user = User::create([
+            'name' => 'admin',
             'email' => 'superAdmin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make("password"),
@@ -28,13 +29,105 @@ class ExamTest extends TestCase
             'code' => null,
         ])->assignRole('superAdmin');
 
+        $this->exam = Exam::factory(1)->create();
+    }
+    public function test_redirect_if_not_authntecated_to_show_exam()
+    { // this function is not in this class
+        $response = $this->get('/exam/index');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_exam_deleted()
+    {
+
+        $this->set_up();
+
+        $this->assertDatabaseHas('exams', $this->exam);
+        $exam = Exam::find(1);
+
+        $response = $this->actingAs($this->user)->delete('exam/delete/1/' . $exam['id']);
+        $response->assertStatus(200);
+        $response->assertStatus(302);
+        $response->assertRedirect('/exam/index/');
+        $this->assertDatabaseMissing('exams', $this->exam);
+
+    }
+
+    public function test_exams_page_not_empty()
+    {
+
+        $this->set_up();
+
+        $exams = Exam::all();
+        $response = $this->actingAs($this->user)->get('/exam/index');
+        $response->assertStatus(200);
+        $response->assertDontSee(__('No Exam'));
+        $response->assertViewHas('exams', function ($collection) use ($exams) {
+            return $collection->contains($exams);
+        });
+
+    }
+
+    public function test_add_exam_edit()
+    {
+        $this->set_up();
+
+
+        $response = $this->actingAs($this->user)->get('/exam/edit/' . $this->exam['id']);
+        $response->assertStatus(200);
+        $response->assertSee('value="' . $this->exam['title'] . '"', false);
+
+    }
+
+    public function test_add_exam_error_redirects_update()
+    {
+        $this->set_up();
+
+        $response = $this->actingAs($this->user)->put('/exam/edit/' . $this->exam['id'], [
+            'title' => ''
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['title']);
+        $response->assertInvalid(['title']);
+    }
+
+    public function test_add_exams_to_database()
+    {
+        $this->set_up();
+
         $response = $this->post('/login', [
             'email' => 'superAdmin@gmail.com',
             'password' => 'password'
         ]);
 
         $response->assertStatus(302);
-        $response->isRedirect('/exam/index');
+        $response->assertRedirect('/exam/create/3');
+
+        $this->assertDatabaseHas('exams', $this->exam[0]);
+
+        $lastExam = Exam::latest()->first();
+
+        $this->assertEquals($lastExam->body, $this->exam['title']);
+
+
+    }
+
+    public function test_exam_delete_successul()
+    {
+        $this->set_up();
+
+        $exam = $this->exam;
+        $response = $this->actingAs($this->user)->delete('/exam/delete/' . $exam['id']);
+        $response->assertStatus(302);
+        $response->assertRedirect('/exam/index/');
+
+        $this->assertDatabaseMissing('exam', $exam[0]);
+        $response->assertDontSee(__('No Exams'));
+        $response->assertViewHas('exams', function ($collection) use ($exam) {
+            return $collection->contains($exam);
+        });
 
     }
 }
--- tests/Feature/OptionTest.php
+++ tests/Feature/OptionTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 class OptionTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
